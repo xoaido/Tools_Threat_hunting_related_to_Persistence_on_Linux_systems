f4a0d50d9a2ce959746add99270f3ea9242c4d73
xoaido <nambeo246@gmail.com>
2023-11-12 08:18:17 +0700

    test5

--- test2_cron.py
+++ test2_cron.py
@@ -1,64 +1,73 @@
 import subprocess
 import re
 
-# Chạy lệnh crontab -l để lấy nội dung crontab hiện tại
-try:
-    crontab_output = subprocess.check_output(["crontab", "-l"], stderr=subprocess.STDOUT, text=True)
-    # print("Nội dung crontab:")
-    # print(crontab_output)
+def crontabScanner():
+    print("\n[*]----------------------[[ CronTab Scan ]]----------------------[*]")
+    # Chạy lệnh crontab -l để lấy nội dung crontab hiện tại
+    try:
+        crontab_output = subprocess.check_output(["crontab", "-l"], stderr=subprocess.STDOUT, text=True)
+        # print("Nội dung crontab:")
+        # print(crontab_output)
+        # Tìm các dòng lập lịch chứa các ký tự đặc biệt hoặc chuỗi cụ thể
+        lines = crontab_output.split('\n')
+        
+        # Tạo từ điển để lưu trữ các danh sách tương ứng
+        matched_lines = {
+            "is_long": [],
+            "is_malicious": [],
+            "is_common_command": [],
+            "is_encoded": [],
+            "is_shell_related": []
+        }
 
-    # Tìm các dòng lập lịch chứa các ký tự đặc biệt hoặc chuỗi cụ thể
-    lines = crontab_output.split('\n')
-    
-    for line in lines:
-        is_malicious = False
-        is_common_command = False
-        is_long = False
-        is_encoded = False
-        is_shell_related = False
-        # Kiểm tra độ dài dòng lập lịch
-        if len(line) > 200:
-            is_long = True
+        for line in lines:
+            is_malicious = False
+            is_common_command = False
+            is_long = False
+            is_encoded = False
+            is_shell_related = False
+            # Kiểm tra độ dài dòng lập lịch
+            if len(line) > 200:
+                is_long = True
 
-        # Kiểm tra nếu dòng lập lịch chứa "/tmp"
-        if "/tmp" in line:
-            is_malicious = True
+            # Kiểm tra nếu dòng lập lịch chứa "/tmp"
+            if "/tmp" in line:
+                is_malicious = True
 
-        # Kiểm tra nếu dòng lập lịch chứa các lệnh "curl", "@", "dig", "http?://*", "nc", "wget"
-        if re.search(r'(curl|@|dig|http\?://\*|nc |wget)', line):
-            is_common_command = True
+            # Kiểm tra nếu dòng lập lịch chứa các lệnh "curl", "@", "dig", "http?://*", "nc", "wget"
+            if re.search(r'(curl|@|dig|http\?://\*|nc |wget)', line):
+                is_common_command = True
 
-        # Kiểm tra nếu dòng lập lịch chứa ký tự mã hóa như "^M" hoặc "base64"
-        if re.search(r'(\^M|base64)', line):
-            is_encoded = True
+            # Kiểm tra nếu dòng lập lịch chứa ký tự mã hóa như "^M" hoặc "base64"
+            if re.search(r'(\^M|base64)', line):
+                is_encoded = True
 
-        if re.search(r'(\|*sh|\*sh -c)', line):
-            is_shell_related = True
+            if re.search(r'(\|*sh|\*sh -c)', line):
+                is_shell_related = True
 
-        if is_long:
-            print("Very long strings, which may indicate encoding:")
-            print(line)
-            print()
-        elif is_malicious:
-            print("Malicious code often exists in this directory:")
-            print(line)
-            print()
-        elif is_common_command:
-            print("These are commonly used commands to connect to the internet:")
-            print(line)
-            print()
-        elif is_encoded:
-            print("Insert and encode commands:")
-            print(line)
-            print()
-        elif is_shell_related:
-            print("Used to run a shell on the system")
-            print(line)
-            print()
-        # else:
-        #     print("Dòng lập lịch nghi ngờ độc hại:")
-        #     print(line)
-        print()
+            # Nếu có bất kỳ dấu hiệu nào được nhận diện, thêm dòng vào danh sách tương ứng
+            if is_long:
+                matched_lines["is_long"].append(line)
+            if is_malicious:
+                matched_lines["is_malicious"].append(line)
+            if is_common_command:
+                matched_lines["is_common_command"].append(line)
+            if is_encoded:
+                matched_lines["is_encoded"].append(line)
+            if is_shell_related:
+                matched_lines["is_shell_related"].append(line)
 
-except subprocess.CalledProcessError as e:
-    print(f"Lỗi: {e.returncode}\n{e.output}")
+        # In tất cả các danh sách tương ứng
+        for key, value in matched_lines.items():
+            if value:
+                print(f"{key.capitalize()}:")
+                for line in value:
+                    print(f"  {line}")
+                    print("-------------------")
+                print()
+
+    except subprocess.CalledProcessError as e:
+        print(f"Lỗi: {e.returncode}\n{e.output}")
+
+# Gọi hàm để kiểm tra crontab
+crontabScanner()
