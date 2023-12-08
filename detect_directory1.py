293a912e8d5b15f84ee76bfcca7d2032c2373d9f
Nguyễn Hữu Bình <98220602+binhnhhe@users.noreply.github.com>
2023-12-08 21:34:43 +0700

    Update detect_directory1.py

--- detect_directory1.py
+++ detect_directory1.py
@@ -15,11 +15,51 @@ suspicious_command_patterns = [
     r"echo\s+.*>\s+.*",
     r"touch\s+.*",
     r"cat\s+.*>>\s+.*",
-    r"nc\s+-lvp\s+.*" ,
+    r"nc\s+-lvp\s+.*",
+    r"dd\s+.*",
     r"mkfs\s+.*",
     r"mknod\s+.*",
     r"tar\s+.*",
-    # Add other persistent attack patterns here
+    r"sudo\s+.*",
+    r"shutdown\s+.*",
+    r"reboot\s+.*",
+    r"kill\s+.*",
+    r"pkill\s+.*",
+    #r"ps\s+.*",
+    #r"top\s+.*",
+    r"ifconfig\s+.*",
+    r"iptables\s+.*",
+    r"useradd\s+.*",
+    r"userdel\s+.*",
+    r"groupadd\s+.*",
+    r"groupdel\s+.*",
+    r"passwds+.*",
+    r"su\s+.*",
+    r"sudoers\s+.*",
+    r"visudo\s+.*",
+    r"crontab\s+.*",
+    #r"at\s+.*",
+    r"systemctl\s+.*",
+    r"journalctl\s+.*",
+    r"service\s+.*",
+    r"init\s+.*",
+    r"ssh\s+.*",
+    r"scp\s+.*",
+    r"rsync\s+.*",
+    r"ftp\s+.*",
+    r"telnet\s+.*",
+    r"nmap\s+.*",
+    r"tcpdump\s+.*",
+    r"wireshark\s+.*",
+    r"traceroute\s+.*",
+    r"ping\s+.*",
+    r"whois\s+.*",
+    r"nslookup\s+.*",
+    r"dig\s+.*",
+    r"host\s+.*",
+    r"curl\s+--proxy\s+.*",
+    r"wget\s+--proxy\s+.*",
+    # Add other suspicious command patterns here
 ]
 
 # List of suspicious patterns for encoding
@@ -49,52 +89,74 @@ suspicious_encoding_patterns = [
     # Add more encoding patterns here
 ]
 
-# Kết hợp các mẫu nghi ngờ từ hai danh sách thành một danh sách duy nhất
+# Combine the suspicious patterns
 suspicious_patterns = suspicious_command_patterns + suspicious_encoding_patterns
 
-# Thực hiện phân tích và phát hiện các tệp script trong thư mục /etc/profile.d/
+# Define the comment pattern
+comment_pattern = r"^\s*#.*"
+
+# Perform identification and analysis of script files in /etc/profile.d/
 script_directory = "/etc/profile.d/"
 script_files = [f for f in os.listdir(script_directory) if os.path.isfile(os.path.join(script_directory, f))]
 
-# Phân tích mã nguồn của từng tệp script
+# Store the results for each script file
+results = []
+
+# Analyze the source code of each script file
 for script_file in script_files:
     script_path = os.path.join(script_directory, script_file)
-    suspicious_commands = []  # Danh sách lệnh nghi ngờ được tìm thấy trong tệp script
-    encoded_chars = []  # Danh sách ký tự được mã hóa được tìm thấy trong tệp script
+
+    # Check if the file has a valid extension
+    file_extension = os.path.splitext(script_file)[1]
+    if file_extension not in ['.sh', '.csh', '.bash', '.ksh', '.zsh']:
+        print(f"File {script_file} has an invalid extension.")
+        continue
+
+    suspicious_commands = []
+    encoded_chars = []
     try:
-        # Đọc nội dung của tệp script
         with open(script_path, 'r', encoding='latin-1') as f:
-            script_code = f.read()
+            script_lines = f.read().splitlines()
+            script_code = ""
+            for line in script_lines:
+                if not re.match(comment_pattern, line):
+                    script_code += line + "\n"
 
-            # Kiểm tra các lệnh nghi ngờ trong mã nguồn script
+            # Check for suspicious commands
             for pattern in suspicious_command_patterns:
-                if re.search(pattern, script_code):
-                    suspicious_commands.append(pattern)
+                matches = re.findall(pattern, script_code)
+                if matches:
+                    for match in matches:
+                        suspicious_commands.append(match.strip())
 
-            # Phát hiện ký tự được mã hóa trong mã nguồn script
+            # Detect encoded characters
             for pattern in suspicious_encoding_patterns:
                 encoded_chars += re.findall(pattern, script_code)
 
     except FileNotFoundError:
-        print(f"File not found: {script_path}")
+        results.append((script_file, "File not found"))
     except UnicodeDecodeError:
-        print(f"Unable to decode file: {script_path}")
+        results.append((script_file, "Unable to decode file"))
 
-    # In kết quả cho mỗi tệp script
-    print(f"Script file: {script_file}")
-    print("-------------------")
-    if suspicious_commands:
-        print("Suspicious commands found:")
-        for command in suspicious_commands:
-            print(command)
-    else:
-        print("No suspicious commands found")
+    # Store the results for each script file
+    if suspicious_commands or encoded_chars:
+        results.append((script_file, suspicious_commands, encoded_chars))
 
-    if encoded_chars:
-        print("Encoded characters found:")
-        for encoded_char in encoded_chars:
-            print(encoded_char)
-    else:
-        print("No encoded characters found")
-
-    print("-------------------")
+# Print the results for each script file
+if results:
+    print("Suspicious files:")
+    for result in results:
+        script_file, suspicious_commands, encoded_chars = result
+        print(f"Script file: {script_file}")
+        print("-------------------")
+        if suspicious_commands:
+            print("Suspicious commands found:")
+            for command in suspicious_commands:
+                print(f"Command: {command}")
+        if encoded_chars:
+            print("Encoded characters found:")
+            for encoded_char in encoded_chars:
+                print(encoded_char)
+        print("-------------------")
+else:
+    print("No suspicious files found")
